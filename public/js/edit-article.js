3eb2fd4321d351a8c400af61650233880b4bb70e
Jorge Cortes <jorge.cortes@gmail.com>
2015-11-08 14:07:58 -0700

    Issue-#9 - Aplicar nuevo diseño a editar artículo
    * Se aplico el nuevo diseño

--- public/js/edit-article.js
+++ public/js/edit-article.js
@@ -20,4 +20,41 @@ $(document).ready(function() {
         $(this).hide();
         $(this).unbind('click');
     });
+
+    $("input:text[name$='title']").simplyCountable({
+        counter: "#counter-title",
+        maxCount: 255,
+        countDirection: 'up',
+        onOverCount: function(count, contable, counter) {
+            counter.parents().eq(2).addClass("has-error");
+            console.log(counter.parents().eq(2));
+        },
+        onSafeCount: function(count, countable, counter) {
+            counter.parents().eq(2).removeClass("has-error");
+        }
+    });
+
+    $("textarea[name$='description']").simplyCountable({
+        counter: "#counter-description",
+        maxCount: 255,
+        countDirection: 'up',
+        onOverCount: function(count, contable, counter) {
+            counter.parents().eq(2).addClass("has-error");
+        },
+        onSafeCount: function(count, countable, counter) {
+            counter.parents().eq(2).removeClass("has-error");
+        }
+    });
+
+    $("input:text[name$='request']").simplyCountable({
+        counter: "#counter-request",
+        maxCount: 255,
+        countDirection: 'up',
+        onOverCount: function(count, contable, counter) {
+            counter.parents().eq(2).addClass("has-error");
+        },
+        onSafeCount: function(count, countable, counter) {
+            counter.parents().eq(2).removeClass("has-error");
+        }
+    });
 });
