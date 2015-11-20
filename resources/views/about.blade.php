f5986893b5b79650dcc506cd473dd3a643d1619c
Jorge Cortes <jorge.cortes@gmail.com>
2015-11-20 16:34:26 -0700

    * Mas sobre acerca de

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -18,4 +18,41 @@ se realiza una oferta, cuanto te sirve o deseas lo que la otra persona tiene par
 
 <p>Recuerda que no existe malos intercambios cuando obtienes lo que deseas sin gastar un solo peso.</p>
 
+<h5>&iquest;Por qué intercambiar cosas?</h5>
+<p>Es simple, muchas veces creemos que siempre ocupamos dinero para lograr algo, pero no debemos olvidar que en nuestro alrededor
+siempre hay cosas de las cuales podemos echar mano para alcanzar lo que queremos, así que en resumidas cuentas:</p>
+<ul>
+    <li>Para obtener algo que deseamos.</li>
+    <li>Para deshacernos de cosas que ya no ocupamos por algo que si ocupamos.</li>
+    <li>Para obtener algún servicio sin gastar.</li>
+    <li>Para adquirir conocimientos.</li>
+</ul>
+
+<h5>Las reglas del juego</h5>
+<ul>
+    <li>Ser honesto.</li>
+    <li>No omitir ningún dato sobre el artículo o servicio que estas publicando.</li>
+    <li>Cumple con tu palabra en todo momento.</li>
+    <li>Promueve los intercambios y el reuso con tus amigos.</li>
+</ul>
+
+<h5>Ejemplo de intercambios</h5>
+<ul>
+    <li>Una laptop por un seminario de negocios con duración de 1 mes.</li>
+    <li>Una subscripción anual en una academia de danza por un paquete de publicidad en radio.</li>
+    <li>Una guitarra por un celular.</li>
+    <li>Asesoría por literaturas diversas.</li>
+</ul>
+
+<h5>&iquest;Quién se puede beneficiar de esto?</h5>
+<ul>
+    <li>Familias.</li>
+    <li>Estudiantes.</li>
+    <li>Viajeros.</li>
+    <li>Escuelas.</li>
+    <li>Amas de casa.</li>
+</ul>
+
+<p>Nos gustaría mucho conocer tus experiencias e historias de tus cambalacheos, si deseas compartirnos tu relato envianos un mensaje
+a través de nuestra forma de <a href="/contact">contacto</a>.</p>
 @endsection
