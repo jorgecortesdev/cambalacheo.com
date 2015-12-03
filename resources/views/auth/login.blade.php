7f23858febaded98939c3fa115e7178cbb3242b1
Jorge Cortes <jorge.cortes@gmail.com>
2015-12-03 15:27:04 -0700

    * Mejoras al disenio responsivo en registro y login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,23 +54,12 @@
         </div>
     </div>
 </div>
-<div class="row">
+<div class="row margin-bottom-20">
     <div class="col-md-12 text-center">
         <a href="/password/email">&iquest;Olvidaste tu contraseña?</a>
     </div>
 </div>
-<br><br>
-<div class="row">
-    <div class="col-md-offset-1 col-md-10 text-center">
-        <p>O si lo prefieres, también puedes acceder a través de tu cuenta de Facebook o Google.</p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-offset-1 col-md-5">
-        <a class="btn btn-lg btn-primary btn-block btn-social btn-facebook" href="{{ url('auth/facebook') }}"><span class="fa fa-facebook"></span> Entrar con Facebook</a>
-    </div>
-    <div class="col-md-5">
-        <a class="btn btn-lg btn-primary btn-block btn-social btn-google" href="{{ url('auth/google') }}"><span class="fa fa-google"></span> Entrar con Google</a>
-    </div>
-</div>
+
+@include('partials.social_buttons')
+
 @endsection
---
+++ resources/views/partials/social_buttons.blade.php
@@ -0,0 +1,15 @@
+<div class="social-buttons">
+    <div class="row">
+        <div class="col-md-offset-1 col-md-10 text-center">
+            <p>O si lo prefieres, también puedes acceder a través de tu cuenta de Facebook o Google.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-offset-1 col-md-5">
+            <a class="margin-bottom-5 btn btn-lg btn-primary btn-block btn-social btn-facebook" href="{{ url('auth/facebook') }}"><span class="fa fa-facebook"></span> Entrar con Facebook</a>
+        </div>
+        <div class="col-md-5">
+            <a class="margin-bottom-5 btn btn-lg btn-primary btn-block btn-social btn-google" href="{{ url('auth/google') }}"><span class="fa fa-google"></span> Entrar con Google</a>
+        </div>
+    </div>
+</div>
