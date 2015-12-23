fdaf74a9a002ec8b7aa696276d328f85930e4247
Jorge Cortes <jorge.cortes@gmail.com>
2015-12-22 20:23:19 -0700

    * Verificacion de Alexa

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
+        <meta name="alexaVerifyID" content="XoBUDlmxZsz6lgTHWhu-SBaZa4c"/>
 
         <title>Cambalacheo - @yield('page_title')</title>
 
